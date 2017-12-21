124a4b95f289be5caaf9f55dbf55168dbe6f7f33
AndrewMElliott <32204180+AndrewMElliott@users.noreply.github.com>
2017-12-20 22:51:44 -0500

    commented parallax code

--- Assets/ParallaxBackground.cs
+++ Assets/ParallaxBackground.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ParallaxBackground : MonoBehaviour {
-	public float parallaxSpeed;
-	public float lastCameraPosX;
-	public float backgroundSize;
+	[SerializeField]
+	private float parallaxSpeed;
+	[SerializeField]
+	private float lastCameraPosX;
+	[SerializeField]
+	private float backgroundSize;
+
 	private Transform cameraTransform;
 	private Transform[] layers;
-	private float viewZone = 10;
+	private float viewBuffer = 10;
 	private int leftIndex;
 	private int rightIndex;
 	// Use this for initialization
@@ -25,19 +29,26 @@ public class ParallaxBackground : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		//Controls the speed of the background.
 		float cameraDeltaX = cameraTransform.position.x - lastCameraPosX;
 		transform.position += Vector3.right * (cameraDeltaX * parallaxSpeed);
+		//set last position of the camera.
 		lastCameraPosX = cameraTransform.position.x;
-
-		if (cameraTransform.position.x < (layers [leftIndex].transform.position.x + viewZone))
+		//If camera center is less than the left most Index [leftIndex, 0, rightIndex] + the viewBuffer length
+		//the screens will treadmill to the left.
+		if (cameraTransform.position.x < (layers [leftIndex].transform.position.x + viewBuffer))
 			ScrollLeft ();
-		if (cameraTransform.position.x > (layers [rightIndex].transform.position.x - viewZone))
+		//If camera center is more than the right most Index [leftIndex, 0, rightIndex] - the viewBuffer length
+		//the screens will treadmill to the right.
+		if (cameraTransform.position.x > (layers [rightIndex].transform.position.x - viewBuffer))
 			ScrollRight ();
+		Debug.Log ("[" + leftIndex + ", " + rightIndex + "]");
 	}
 
 	private void ScrollLeft(){
-		int lastRight = rightIndex;
+		//calculates coordinates past the left most background Index position [leftIndex, 0, rightIndex].
 		layers [rightIndex].position = Vector3.right * (layers [leftIndex].position.x - backgroundSize);
+		//rotate tracked indexes (leftIndex/rightIndex). [2,1] -> [1,0] -> [0,2] -> [2,1]
 		leftIndex = rightIndex;
 		rightIndex--;
 		if (rightIndex < 0)
@@ -45,8 +56,9 @@ public class ParallaxBackground : MonoBehaviour {
 	}
 
 	private void ScrollRight(){
-		int lastLeft = leftIndex;
+		//positions the left most background Index [leftIndex, 0, rightIndex] to the right most Index.
 		layers [leftIndex].position = Vector3.right * (layers [rightIndex].position.x + backgroundSize);
+		//rotate tracked indexes (leftIndex/rightIndex). [0,2] -> [1,0] -> [2,1] -> [0,2]
 		rightIndex = leftIndex;
 		leftIndex++;
 		if (leftIndex == layers.Length )
