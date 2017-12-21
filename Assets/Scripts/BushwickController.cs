54b64ad15df85425a7cf14a43ef7cbe8b631d8c1
unknown <jeff.oh647@gmail.com>
2017-12-20 19:26:37 -0500

    Player movement and camera clamp added

---
+++ Assets/Scripts/BushwickController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BushwickController : MonoBehaviour {
+    [SerializeField]
+    float Speed = 8f;
+    [SerializeField]
+    float jumpForce = 450f;
+    [SerializeField]
+    float fallMultiplier = 1.8f;
+
+    bool facingRight = true;
+    private Animator bushwickAnimation;
+    private Rigidbody2D _rigidBody;
+
+    bool onGround = false;
+    public Transform onGroundCheck;
+    float onGroundRadius = 0.2f;
+    public LayerMask IsGround;
+
+    // Use this for initialization
+    void Start() {
+        bushwickAnimation = GetComponent<Animator>();
+        _rigidBody = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    //From https://unity3d.com/learn/tutorials/topics/2d-game-creation/2d-character-controllers and Blackboard content
+    void FixedUpdate() {
+        onGround = Physics2D.OverlapCircle(onGroundCheck.position, onGroundRadius, IsGround);
+        bushwickAnimation.SetBool("Ground", onGround);
+
+        float userMove = Input.GetAxis("Horizontal");
+        bushwickAnimation.SetFloat("Speed", Mathf.Abs(userMove));
+
+        _rigidBody.velocity = new Vector2(userMove * Speed, _rigidBody.velocity.y);
+
+        if (_rigidBody.velocity.x > 0)
+        {
+            gameObject.transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (_rigidBody.velocity.x < 0)
+        {
+            gameObject.transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+    }
+
+    void Update()
+    {
+        
+        if (onGround && Input.GetButtonDown("Jump"))
+        {
+            bushwickAnimation.SetBool("Ground", false);
+            _rigidBody.AddForce(new Vector2(0, jumpForce));
+        }
+
+        //When falling fall down faster From https://www.youtube.com/watch?v=7KiK0Aqtmzc
+        if (!onGround && _rigidBody.velocity.y < 0)
+        {
+            _rigidBody.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+    }
+    //From https://unity3d.com/learn/tutorials/topics/2d-game-creation/2d-character-controllers and Blackboard content
+
+
+
+}
---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    private GameObject player;
+
+    [SerializeField]
+    float minX;
+    [SerializeField]
+    float maxX;
+    [SerializeField]
+    float minY;
+    [SerializeField]
+    float maxY;
+    [SerializeField]
+    float playerShift = 2f;
+
+
+    // Use this for initialization
+    void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float x = Mathf.Clamp(player.transform.position.x, minX + playerShift, maxX);
+        float y = Mathf.Clamp(player.transform.position.y, minY, maxY);
+        gameObject.transform.position = new Vector3(x + playerShift, y, gameObject.transform.position.z);
+    }
+}
