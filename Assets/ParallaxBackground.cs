33d1292e50c2827412f1a108770b95812081cb2a
AndrewMElliott <32204180+AndrewMElliott@users.noreply.github.com>
2017-12-20 21:28:15 -0500

    added Parallax background

---
+++ Assets/ParallaxBackground.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxBackground : MonoBehaviour {
+	public float parallaxSpeed;
+	public float lastCameraPosX;
+	public float backgroundSize;
+	private Transform cameraTransform;
+	private Transform[] layers;
+	private float viewZone = 10;
+	private int leftIndex;
+	private int rightIndex;
+	// Use this for initialization
+	void Start () {
+		
+		cameraTransform = Camera.main.transform;
+		lastCameraPosX = cameraTransform.position.x;
+		layers = new Transform[transform.childCount];
+		for (int i = 0; i < transform.childCount; i++)
+			layers [i] = transform.GetChild (i);
+		leftIndex = 0;
+		rightIndex = layers.Length - 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float cameraDeltaX = cameraTransform.position.x - lastCameraPosX;
+		transform.position += Vector3.right * (cameraDeltaX * parallaxSpeed);
+		lastCameraPosX = cameraTransform.position.x;
+
+		if (cameraTransform.position.x < (layers [leftIndex].transform.position.x + viewZone))
+			ScrollLeft ();
+		if (cameraTransform.position.x > (layers [rightIndex].transform.position.x - viewZone))
+			ScrollRight ();
+	}
+
+	private void ScrollLeft(){
+		int lastRight = rightIndex;
+		layers [rightIndex].position = Vector3.right * (layers [leftIndex].position.x - backgroundSize);
+		leftIndex = rightIndex;
+		rightIndex--;
+		if (rightIndex < 0)
+			rightIndex = layers.Length - 1;
+	}
+
+	private void ScrollRight(){
+		int lastLeft = leftIndex;
+		layers [leftIndex].position = Vector3.right * (layers [rightIndex].position.x + backgroundSize);
+		rightIndex = leftIndex;
+		leftIndex++;
+		if (leftIndex == layers.Length )
+			leftIndex = 0;
+	}
+}
