f5acf0bef4a25a53f6d1963074c821b4c12d82fd
AndrewMElliott <32204180+AndrewMElliott@users.noreply.github.com>
2017-12-22 01:15:28 -0500

    added main menu and pause script

---
+++ Assets/Scripts/NormalScrollingBackground.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NormalScrollingBackground : MonoBehaviour {
+	[SerializeField]
+	private float scrollSpeed;
+	[SerializeField]
+	private float endPointX;
+
+	[SerializeField]
+	private float repositionX;
+
+
+	void Update () {
+		Vector2 position = new Vector2 (transform.position.x - scrollSpeed * Time.deltaTime, transform.position.y);
+		transform.position = position;
+
+		if (transform.position.x <= endPointX)
+			transform.position = new Vector2 (repositionX, transform.position.y);
+	}
+}
---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+	[SerializeField]
+	private GameObject pauseMenu;
+	private bool isActive = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Escape)){
+			isActive = !isActive;
+			pauseMenu.SetActive(isActive);
+			if (isActive)
+				Time.timeScale = 0;
+			else
+				Time.timeScale = 1;
+		}
+	}
+}
