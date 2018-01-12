9db530ce6cd8678f263d0de41d5da8ea48848828
AndrewMElliott <32204180+AndrewMElliott@users.noreply.github.com>
2018-01-12 11:39:50 -0500

    added audio

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+	private AudioSource grenade;
+	private AudioSource gun;
+
+	// Use this for initialization
+	void Start () {
+		DontDestroyOnLoad (this.gameObject);
+		AudioSource[] audio = this.transform.GetComponents<AudioSource>();
+		grenade = audio [2];
+		gun = audio [1];
+
+	}
+	
+	public void PlayGun(){
+		gun.Play ();
+	}
+	public void PlayBoom(){
+		grenade.Play ();
+	}
+}
